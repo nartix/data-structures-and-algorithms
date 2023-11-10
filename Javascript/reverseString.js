ae5d0b9c5934d920ed6a37c7bcf0475e5bb307b4
nartix <nartix@gmail.com>
2023-11-10 08:46:41 +0000

    mergeSortedArray complete

--- Javascript/reverseString.js
+++ Javascript/reverseString.js
@@ -40,3 +40,26 @@ const reverseString4 = (string) => string.split('').reverse().join('');
 const reverseString5 = (string) => [...string].reverse().join('');
 
 console.log(reverseString3('Hello World'));
+
+// from course
+function reverse(str) {
+  if (!str || typeof str != 'string' || str.length < 2) return str;
+
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i]);
+  }
+  return backwards.join('');
+}
+
+function reverse2(str) {
+  //check for valid input
+  return str.split('').reverse().join('');
+}
+
+const reverse3 = (str) => [...str].reverse().join('');
+
+reverse('Timbits Hi');
+reverse('Timbits Hi');
+reverse3('Timbits Hi');
